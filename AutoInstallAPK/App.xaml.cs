27fd2f3b5f94a530e4478df844734c792d9c937a
孙昊哲 <sun2000nian@live.com>
2021-10-22 10:25:19 +0800

    fileopenfinish

--- AutoInstallAPK/App.xaml.cs
+++ AutoInstallAPK/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -95,6 +96,40 @@ namespace AutoInstallAPK
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: 保存应用程序状态并停止任何后台活动
             deferral.Complete();
+        }
+
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            // TODO: Handle file activation
+
+            // The number of files received is args.Files.Size
+            // The first file is args.Files[0].Name
+            ApplicationData.Current.LocalSettings.Values["openningPath"] = args.Files[0].Path;
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            // 不要在窗口已包含内容时重复应用程序初始化，
+            // 只需确保窗口处于活动状态
+            if (rootFrame == null)
+            {
+                // 创建要充当导航上下文的框架，并导航到第一页
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // 将框架放在当前窗口中
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // 当导航堆栈尚未还原时，导航到第一页，
+                // 并通过将所需信息作为导航参数传入来配置
+                // 参数
+                rootFrame.Navigate(typeof(InstallPage));
+            }
+            // 确保当前窗口处于活动状态
+            Window.Current.Activate();
+            //RunCommand.Run("C:\\Users\\sun20\\Desktop\\test\\default_test.exe", args.Files[0].Name);
         }
     }
 }
